91fa1a1880726826e13a9e645df3949f21745a4d
Rafael Feijo <rfeijo@smartgreen.net>
2015-12-08 15:40:10 -0200

    CHANGE Aplicado nova propaganda para gatry

--- url.py
+++ url.py
@@ -10,11 +10,21 @@ from urllib2 import Request, urlopen
 import sqlite3
 import pwd
 import os
+import sys
 
 
 class Url(object):
 
     conn = None
+
+    hdr = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'
+    }
 
     def connect_db(self):
 
@@ -76,16 +86,9 @@ class Url(object):
 
         self.conn.commit()
 
-    def start(self):
+    def hardmob(self):
 
         url = 'http://www.hardmob.com.br/promocoes/'
-
-        hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-               'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-               'Accept-Encoding': 'none',
-               'Accept-Language': 'en-US,en;q=0.8',
-               'Connection': 'keep-alive'}
 
         # conectou
         self.connect_db()
@@ -93,7 +96,7 @@ class Url(object):
         # populou
         self.population_db('hardmob')
 
-        req = Request(url, headers=hdr)
+        req = Request(url, headers=self.hdr)
 
         page = urlopen(req).read()
 
@@ -114,6 +117,48 @@ class Url(object):
                 )
                 notif.show()
 
+    def gatry(self):
+
+        url = 'http://gatry.com/'
+
+        # conectou
+        self.connect_db()
+
+        # populou
+        self.population_db('gatry')
+
+        req = Request(url, headers=self.hdr)
+
+        page = urlopen(req).read()
+
+        soup = BeautifulSoup(page, 'html.parser')
+        soup.prettify()
+
+        # initialisation de libnotify avec le nom de notre application
+        Notify.init(u'Promoções Gatry')
+
+        for s in soup.find_all("article", class_="promocao"):
+            # inseriu
+            pricec, price = s.find_all("p", class_="preco")[0].find_all("span")
+            value = "%s %s" % (pricec.string, price.string)
+            for h3 in s.find_all("h3"):
+                for a in h3.find_all("a"):
+                    title = "(%s) -> %s" % (value, a.string)
+                    if not self.get_db_item('gatry', title):
+                        self.insert_db('gatry', (title, a['href']))
+                        notif = Notify.Notification.new(
+                            '%s' % title,
+                            '%s' % a['href'],
+                            'dialog-information'
+                        )
+                        notif.show()
+
+# default
+method = 'hardmob'
+
+if len(sys.argv) > 1:
+    method = sys.argv[1]
+
 url = Url()
 # iniciar busca
-url.start()
+getattr(url, method)()
